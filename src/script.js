940ded2a0224f373f12cc6f5df3aa4b382e942fc
Franz2610 <francescofroechlich1@gmail.com>
2023-03-30 16:41:56 +0200

    modifiche

--- src/script.js
+++ src/script.js
@@ -10,29 +10,46 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            listaAzioni : [
-                'Finire esercizio',
-                'Ripetere',
-                'Cucinare',
-                'Palestra',
-                'Calcio'
+            listaAzioni : [{
+                name : 'Finire esercizio',
+                completed : false
+            },{
+                name : 'Ripetere',
+                completed : false
+            },{
+                name : 'Cucinare',
+                completed : false
+            },{
+                name : 'Palestra',
+                completed : false
+            },{
+                name : 'Cenare',
+                completed : false
+            },{
+                name : 'Giocare a calcio',
+                completed : false
+            },
             ]
+            nuovaAzione : '',
             
             }
         },
         methods:{
             inserisci(){
-
-                this.listaAzioni.push(this.text);
+                const newAction = {
+                    name : this.nuovaAzione,
+                    completed : false
+                }
+                this.listaAzioni.push(newAction);
+                this.nuovaAzione = ''
             
             },
             cancella(index){
                 this.listaAzioni.splice(index, 1)
             },
-            //completed(index){
-                //this.listaSpesa[index].completed = true;
-
-           // }
+            completed(index){
+                this.listaSpesa[index].completed = true;
+            }
 
         },
         mounted() {
